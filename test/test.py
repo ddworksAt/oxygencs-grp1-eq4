3532a6a7c6b42cf1b987ac0fd2ddbdfaef124ca0
David Do <david.do.1@ens.etsmtl.ca>
2024-02-28 15:56:53 -0500

    Tested on_receive's calls to other functions

--- test/test.py
+++ test/test.py
@@ -1,41 +1,21 @@
 import unittest
 from unittest.mock import MagicMock
 from src.main import App
-import os
-from dotenv import load_dotenv
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = App()
-        load_dotenv()
-
-        self.app.HOST = os.getenv('HOST')
-        self.app.TOKEN = os.getenv('TOKEN')
-        self.app.T_MAX = os.getenv('T_MAX')
-        self.app.T_MIN = os.getenv('T_MIN')
-        self.app.DATABASE_URL = os.getenv('DATABASE_URL')
-        self.app.MIN_CONN = os.getenv('MIN_CONN')
-        self.app.MAX_CONN = os.getenv('MAX_CONN')
         self.app.connection_pool = MagicMock()
         self.app.connection_pool.getconn = MagicMock(return_value=MagicMock())
         self.app.connection_pool.putconn = MagicMock()
 
-    # def test_on_sensor_data_received(self):
-    #     self.app.save_event_to_database = MagicMock()
-
-    #     def side_effect(temperature):
-    #         if float(temperature) >= float(self.app.T_MAX):
-    #             return "Activating AC for 10 ticks"
-    #         elif float(temperature) <= float(self.app.T_MIN):
-    #             return "Activating Heater for 10 ticks"
-    #         else:
-    #             return None
-    #     self.app.take_action = MagicMock(side_effect = side_effect)
+    def test_on_sensor_data_received(self):
+        self.app.take_action = MagicMock()
+        self.app.save_event_to_database = MagicMock()
         
-    #     self.app.on_sensor_data_received([{"date": "2020-01-01", "data": "25.0"}])
-    #     self.app.take_action.assert_called_once_with(25.0)
-    #     self.assertEqual(self.app.take_action.return_value, "Activating AC for 10 ticks")
-    #     self.app.save_event_to_database.assert_called_once_with("2020-01-01", 25.0, self.app.take_action.return_value)
+        self.app.on_sensor_data_received([{"date": "2020-01-01", "data": "25.0"}])
+        self.app.take_action.assert_called_once
+        self.app.save_event_to_database.assert_called_once
 
     def test_take_action(self):
         self.assertEqual(self.app.take_action(18.0), "Activating Heater for 10 ticks")
@@ -51,11 +31,11 @@ class TestApp(unittest.TestCase):
         self.app.put_connection = MagicMock()
 
         self.app.save_event_to_database("2024-01-01", 18.0, "Activating Heater for 10 ticks")
-        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
         self.app.get_connection.assert_called_once()
         self.app.put_connection.assert_called_once()
         self.app.get_connection.return_value.cursor.assert_called()
         self.app.get_connection.return_value.commit.assert_called_once()
+        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
         
         self.app.save_event_to_database("2024-01-01", 19.0, "Activating Heater for 10 ticks")
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
