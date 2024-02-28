4bbf4f8cfc648096f841520983678c7a42ccb920
David Do <david.do.1@ens.etsmtl.ca>
2024-02-28 14:31:09 -0500

    Auto stash before revert of "Testing insert in db and updating Pipfile.lock"

--- test/test.py
+++ test/test.py
@@ -20,7 +20,6 @@ class TestApp(unittest.TestCase):
         self.app.connection_pool.getconn = MagicMock(return_value=MagicMock())
         self.app.connection_pool.putconn = MagicMock()
 
-# make more interesting tests for on_sensor_data_received...
     # def test_on_sensor_data_received(self):
     #     self.app.take_action = MagicMock()
     #     def side_effect(temperature):
@@ -45,11 +44,17 @@ class TestApp(unittest.TestCase):
     def test_save_event_to_database(self):
         self.app.get_connection = MagicMock(return_value=MagicMock())
         self.app.put_connection = MagicMock()
+        
         self.app.save_event_to_database("2024-01-01", 25.0, "Activating AC for 10 ticks")
         self.app.get_connection.assert_called_once()
         self.app.put_connection.assert_called_once()
         self.app.get_connection.return_value.cursor.assert_called()
-        # since 25.0 exceeds T_MAX = 24, the last cursor call is with the events insert. add asserts for various temperature values
-        # self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (25.0, "2024-01-01"))
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating AC for 10 ticks", "2024-01-01"))
         self.app.get_connection.return_value.commit.assert_called_once()
+
+        self.app.save_event_to_database("2024-01-01", 18.0, "Activating Heater for 10 ticks")
+        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
+
+        self.app.save_event_to_database("2024-01-01", 22.0, None)
+        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (22.0, "2024-01-01"))
+        
