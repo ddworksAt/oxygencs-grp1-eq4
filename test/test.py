7ed98cfc438c1611cfd2db822ba8eb53f3abb335
David Do <david.do.1@ens.etsmtl.ca>
2024-02-28 16:49:14 -0500

    quick edit for tests

--- test/test.py
+++ test/test.py
@@ -1,13 +1,31 @@
 import unittest
 from unittest.mock import MagicMock
 from src.main import App
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = App()
+
+        load_dotenv()
+        self.app.HOST = os.getenv('HOST')
+        self.app.TOKEN = os.getenv('TOKEN')
+        self.app.T_MIN = os.getenv('T_MIN')
+        self.app.T_MAX = os.getenv('T_MAX')
+
         self.app.connection_pool = MagicMock()
         self.app.connection_pool.getconn = MagicMock(return_value=MagicMock())
         self.app.connection_pool.putconn = MagicMock()
+
+    def test_db_connection(self):
+        try:
+            conn = psycopg2.connect(self.app.DATABASE_URL)
+            self.assertIsNotNone(conn)
+            conn.close()
+        except:
+            self.fail("Database connection failed")
 
     def test_on_sensor_data_received(self):
         self.app.take_action = MagicMock()
@@ -18,13 +36,13 @@ class TestApp(unittest.TestCase):
         self.app.save_event_to_database.assert_called_once
 
     def test_take_action(self):
-        self.assertEqual(self.app.take_action(18.0), "Activating Heater for 10 ticks")
-        self.assertEqual(self.app.take_action(19.0), "Activating Heater for 10 ticks")
-        self.assertEqual(self.app.take_action(19.1), None)
-        self.assertEqual(self.app.take_action(22.0), None)
-        self.assertEqual(self.app.take_action(23.9), None)
-        self.assertEqual(self.app.take_action(24.0), "Activating AC for 10 ticks")
-        self.assertEqual(self.app.take_action(25.0), "Activating AC for 10 ticks")
+        # self.assertEqual(self.app.take_action(self.app.T_MIN - 1), "Activating Heater for 10 ticks")
+        self.assertEqual(self.app.take_action(float(self.app.T_MIN)), "Activating Heater for 10 ticks")
+        self.assertEqual(self.app.take_action(float(self.app.T_MIN) + 0.1), None)
+        self.assertEqual(self.app.take_action(float(self.app.T_MIN) + 1), None)
+        self.assertEqual(self.app.take_action(float(self.app.T_MAX) - 0.1), None)
+        self.assertEqual(self.app.take_action(float(self.app.T_MAX)), "Activating AC for 10 ticks")
+        self.assertEqual(self.app.take_action(float(self.app.T_MAX) + 1), "Activating AC for 10 ticks")
 
     def test_save_event_to_database(self):
         self.app.get_connection = MagicMock(return_value=MagicMock())
@@ -37,20 +55,20 @@ class TestApp(unittest.TestCase):
         self.app.get_connection.return_value.commit.assert_called_once()
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
         
-        self.app.save_event_to_database("2024-01-01", 19.0, "Activating Heater for 10 ticks")
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MIN), "Activating Heater for 10 ticks")
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating Heater for 10 ticks", "2024-01-01"))
 
-        self.app.save_event_to_database("2024-01-01", 19.1, None)
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MIN) + 0.1, None)
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (19.1, "2024-01-01"))
 
-        self.app.save_event_to_database("2024-01-01", 22.0, None)
-        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (22.0, "2024-01-01"))
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MIN) + 1, None)
+        self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (20.0, "2024-01-01"))
         
-        self.app.save_event_to_database("2024-01-01", 23.9, None)
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MAX) - 0.1, None)
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO temperatures (temperature, "createdAt") VALUES (%s, %s)', (23.9, "2024-01-01"))
 
-        self.app.save_event_to_database("2024-01-01", 24.0, "Activating AC for 10 ticks")
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MAX), "Activating AC for 10 ticks")
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating AC for 10 ticks", "2024-01-01"))
 
-        self.app.save_event_to_database("2024-01-01", 25.0, "Activating AC for 10 ticks")
+        self.app.save_event_to_database("2024-01-01", float(self.app.T_MAX) + 1, "Activating AC for 10 ticks")
         self.app.get_connection.return_value.cursor.return_value.execute.assert_called_with('INSERT INTO events (event, "createdAt") VALUES (%s, %s)', ("Activating AC for 10 ticks", "2024-01-01"))
